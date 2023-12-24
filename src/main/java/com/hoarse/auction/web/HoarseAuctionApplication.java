1c4013beb3f2a9ddbe73d78aac6375c9e9811933
Kim-Dayeong <kkimdayeongg@gmail.com>
2023-12-24 20:11:10 +0900

    commit

---
+++ src/main/java/com/hoarse/auction/web/HoarseAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.hoarse.auction.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoarseAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoarseAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoarse/auction/web/HoarseAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoarse.auction.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoarseAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
